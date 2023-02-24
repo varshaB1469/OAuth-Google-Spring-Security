50ac4de325ec1f72e8950826297235312b64dffd
varshaB1469 <108652539+varshaB1469@users.noreply.github.com>
2023-02-24 21:56:45 +0530

    Delete Ecommerce-youtube directory

--- Ecommerce-youtube/src/main/java/com/example/ecommerce/repositories/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ecommerce.model.Order;
-
-public interface OrderRepo extends JpaRepository<Order, Integer>{
-
-}
--- Ecommerce-youtube/src/main/java/com/example/ecommerce/repositories/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ecommerce.model.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Integer>{
-
-}
